ac105b5478ed8ce590bb77a63996e2cde9722a9d
ardans <ardans@foxmail.com>
2024-07-31 09:51:36 +0800

    chore: update deps

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,4 +3,9 @@ import antfu from '@antfu/eslint-config'
 export default antfu({
   formatters: true,
   unocss: true,
+  stylistic: {
+    indent: 2, // 4, or 'tab'
+    quotes: 'single', // or 'double'
+  },
+  typescript: true,
 })
